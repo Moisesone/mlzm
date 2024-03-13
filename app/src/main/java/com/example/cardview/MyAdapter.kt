7f5409ed795ec8abbdd8b44f44d12405702437ed
Moisesone <moises.pena0176@alumnos.udg.mx>
2024-03-12 19:23:47 -0600

    Initial commit

---
+++ app/src/main/java/com/example/cardview/MyAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.cardview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter(private val dataSet: List<String>) :
+    RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.textView)
+        val cardView: CardView = view.findViewById(R.id.cardView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_cardview, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = dataSet[position]
+    }
+
+    override fun getItemCount() = dataSet.size
+}
